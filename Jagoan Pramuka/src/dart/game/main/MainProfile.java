f8d0605a464e7620b5753d51941e41e0b09d5ac3
Robeth Rahmatullah <robethrahmatullah@gmail.com>
2012-08-11 22:10:41 +0700

    Add interface to save persistent state for Shop & Level.

--- Jagoan Pramuka/src/dart/game/main/MainProfile.java
+++ Jagoan Pramuka/src/dart/game/main/MainProfile.java
@@ -11,12 +11,32 @@ import java.io.DataOutputStream;
 
 /**
  *
- * @author Agung
+ * @author Robeth Rahmatullah
  */
 public class MainProfile extends Profile{
 
-    private boolean sound = false;
+    /**
+     * Setting variables (Sound)
+     */
+    private boolean sound;
+    
+    /**
+     * Level informations
+     */
+    private int lastLevel;
+    private int[] bestCombo, highscore, savedAnimals;
+    
+    /**
+     * Shop Variables
+     */
+    private int money;
+    private boolean hasTongkat, hasTali, hasKitchenKit;
+    private boolean hasAcc1, hasAcc2, hasAcc3;
+
+    
+    
     private int score = 0;
+    
 
     public MainProfile(){
     }
@@ -54,4 +74,103 @@ public class MainProfile extends Profile{
         return sound;
     }
 
+    public boolean isHasAcc1() {
+        return hasAcc1;
+    }
+
+    public void setHasAcc1(boolean hasAcc1) {
+        this.hasAcc1 = hasAcc1;
+    }
+
+    public boolean isHasAcc2() {
+        return hasAcc2;
+    }
+
+    public void setHasAcc2(boolean hasAcc2) {
+        this.hasAcc2 = hasAcc2;
+    }
+
+    public boolean isHasAcc3() {
+        return hasAcc3;
+    }
+
+    public void setHasAcc3(boolean hasAcc3) {
+        this.hasAcc3 = hasAcc3;
+    }
+
+    public boolean isHasKitchenKit() {
+        return hasKitchenKit;
+    }
+
+    public void setHasKitchenKit(boolean hasKitchenKit) {
+        this.hasKitchenKit = hasKitchenKit;
+    }
+
+    public boolean isHasTali() {
+        return hasTali;
+    }
+
+    public void setHasTali(boolean hasTali) {
+        this.hasTali = hasTali;
+    }
+
+    public boolean isHasTongkat() {
+        return hasTongkat;
+    }
+
+    public void setHasTongkat(boolean hasTongkat) {
+        this.hasTongkat = hasTongkat;
+    }
+
+    public int getLastLevel() {
+        return lastLevel;
+    }
+
+    public void setLastLevel(int lastLevel) {
+        this.lastLevel = lastLevel;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+    
+    public int[] getHighscore(){
+        return highscore;
+    }
+    
+    public int[] getBestCombo(){
+        return bestCombo;
+    }
+    
+    public int[] getSavedAnimals(){
+        return savedAnimals;
+    }
+    
+    public int getHighscore(int level){
+        return highscore[level];
+    }
+    
+    public int getBestCombo(int level){
+        return bestCombo[level];
+    }
+    
+    public int getSavedAnimals(int level){
+        return savedAnimals[level];
+    }
+    
+    public void setHighscore(int level, int newHighscore){
+        highscore[level] = newHighscore;
+    }
+    
+    public void setBestCombo(int level, int newBestCombo){
+        bestCombo[level] = newBestCombo;
+    }
+    
+    public void setSavedAnimals(int level, int animalsRecord){
+        savedAnimals[level] = animalsRecord;
+    }
 }
