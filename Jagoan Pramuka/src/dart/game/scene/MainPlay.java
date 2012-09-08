661a7b58a180b31624e3c325eb55a56a4dffcd9b
Robeth Rahmatullah <robethrahmatullah@gmail.com>
2012-09-08 22:05:08 +0700

    Tambah Animasi Tutorial

--- Jagoan Pramuka/src/dart/game/scene/MainPlay.java
+++ Jagoan Pramuka/src/dart/game/scene/MainPlay.java
@@ -29,7 +29,6 @@ public class MainPlay extends Scene {
             WIN_STATE = 20998,
             LOSE_STATE = 1019,
             PRE_STATE = 9090;
-    
     public static final String AYAM = "/1_ayam.png",
             KAMBING = "/2_kambing.png",
             SAPI = "/3_sapi.png",
@@ -60,7 +59,6 @@ public class MainPlay extends Scene {
             CENDRAWASIH_NO = "13_cendrawasih_no.png",
             KOMODO_NO = "14_komodo_no.png",
             GAJAH_NO = "15_gajah_no.png";
-    
     private Image aryaImage,
             bimaImage,
             cintaImage,
@@ -99,7 +97,9 @@ public class MainPlay extends Scene {
             ANIM1 = 0,
             ANIM2 = 1,
             ANIM3 = 2;
-    
+    private Image currentTutorialImage;
+    private int currentTutorialIndex;
+    private boolean isTutorial;
 
     MainPlay(Engine engine, int level) {
         super(engine);
@@ -119,7 +119,7 @@ public class MainPlay extends Scene {
             twoImage = Image.createImage("/2.png");
             threeImage = Image.createImage("/3.png");
             startImage = Image.createImage("/semangat.png");
-            
+
 
             heroes = new Hero[3];
             heroes[0] = new Hero(aryaImage, 50, 43, 200, Hero.ARYA);
@@ -141,25 +141,26 @@ public class MainPlay extends Scene {
             postState = SCORE;
             curScore = 0;
             timer = new Timer(1000);
-            
+
             System.out.println("LEvel :" + level);
-            if (level == 1){
-                hud = new HUD(berlin, gothic, profile.getMoney(),AYAM,KAMBING,SAPI,AYAM_NO,KAMBING_NO,SAPI_NO);
-            } else if (level == 2){
-                hud = new HUD(berlin, gothic, profile.getMoney(),ANOA,BABIRUSA,BEKANTAN,ANOA_NO,BABIRUSA_NO,BEKANTAN_NO);
-            } else if (level == 3){
-                hud = new HUD(berlin, gothic, profile.getMoney(),BERUANG_MADU,ENGGANG,BINTURUNG,BERUANG_MADU_NO,ENGGANG_NO,BINTURUNG_NO);
-            } else if (level == 4){
-                hud = new HUD(berlin, gothic, profile.getMoney(),BADAK,MALEO,HARIMAU,BADAK_NO,MALEO_NO,HARIMAU_NO);
-            } else if (level == 5){
-                hud = new HUD(berlin, gothic, profile.getMoney(),CENDRAWASIH,KOMODO,GAJAH,CENDRAWASIH_NO,KOMODO_NO,GAJAH_NO);
+            if (level == 1) {
+                hud = new HUD(berlin, gothic, profile.getMoney(), AYAM, KAMBING, SAPI, AYAM_NO, KAMBING_NO, SAPI_NO);
+            } else if (level == 2) {
+                hud = new HUD(berlin, gothic, profile.getMoney(), ANOA, BABIRUSA, BEKANTAN, ANOA_NO, BABIRUSA_NO, BEKANTAN_NO);
+            } else if (level == 3) {
+                hud = new HUD(berlin, gothic, profile.getMoney(), BERUANG_MADU, ENGGANG, BINTURUNG, BERUANG_MADU_NO, ENGGANG_NO, BINTURUNG_NO);
+            } else if (level == 4) {
+                hud = new HUD(berlin, gothic, profile.getMoney(), BADAK, MALEO, HARIMAU, BADAK_NO, MALEO_NO, HARIMAU_NO);
+            } else if (level == 5) {
+                hud = new HUD(berlin, gothic, profile.getMoney(), CENDRAWASIH, KOMODO, GAJAH, CENDRAWASIH_NO, KOMODO_NO, GAJAH_NO);
             }
-            
-            world = new World(this, heroes, hud,level);
-            
-            
-                 
-            
+
+            world = new World(this, heroes, hud, level);
+            isTutorial = level == 1;
+            currentTutorialIndex = 0;
+            if (isTutorial) {
+                currentTutorialImage = getTutorialImage(0);
+            }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
@@ -178,106 +179,115 @@ public class MainPlay extends Scene {
     }
 
     public void update(long currentTime) {
-        if (gameState == PLAY_STATE) {
-            updateKeyStates();
-            world.update(currentTime, keyDownStates);
-            hud.update(world.getComboObj(), world.getFaObj());
-        } else if (gameState == PRE_STATE) {
-            if (preState == ZERO) {
-                timer.start();
-                preState = ONE;
-            } else if (timer.isTicked(currentTime)) {
-                timer.reset();
-                if (++preState > START) {
-                    gameState = PLAY_STATE;
-                }
-            }
-        } else if (isGameEnd()) {
-            if (postState == SCORE) {
-                if (curScore < hud.getScore()) {
-                    curScore++;
-                } else {
-                    postState = COMBO;
+        if (isTutorial) {
+        } else {
+            if (gameState == PLAY_STATE) {
+                updateKeyStates();
+                world.update(currentTime, keyDownStates);
+                hud.update(world.getComboObj(), world.getFaObj());
+            } else if (gameState == PRE_STATE) {
+                if (preState == ZERO) {
+                    timer.start();
+                    preState = ONE;
+                } else if (timer.isTicked(currentTime)) {
                     timer.reset();
+                    if (++preState > START) {
+                        gameState = PLAY_STATE;
+                    }
                 }
-            } else if (postState == COMBO) {
-                if (timer.isTicked(currentTime)) {
-                    postState = ANIMAL;
-                    animCounter = ANIM1;
-                    timer.reset();
+            } else if (isGameEnd()) {
+                if (postState == SCORE) {
+                    if (curScore < hud.getScore()) {
+                        curScore++;
+                    } else {
+                        postState = COMBO;
+                        timer.reset();
+                    }
+                } else if (postState == COMBO) {
+                    if (timer.isTicked(currentTime)) {
+                        postState = ANIMAL;
+                        animCounter = ANIM1;
+                        timer.reset();
 
-                }
-            } else if (postState == ANIMAL) {
-                if (timer.isTicked(currentTime)) {
-                    timer.reset();
-                    animCounter++;
-                    if (animCounter > ANIM3) {
-                        postState = CONFIRM;
+                    }
+                } else if (postState == ANIMAL) {
+                    if (timer.isTicked(currentTime)) {
+                        timer.reset();
+                        animCounter++;
+                        if (animCounter > ANIM3) {
+                            postState = CONFIRM;
+                        }
                     }
                 }
             }
         }
-
     }
 
     public void paint(Graphics g) {
         try {
-            background.setPosition(world.getDeltaX(), world.getDeltaY());
-            background.paint(g);
-            world.paint(g);
-            hud.paint(g);
+            if (isTutorial) {
+                g.drawImage(currentTutorialImage, 0, 0, Graphics.LEFT | Graphics.TOP);
+            } else {
+                background.setPosition(world.getDeltaX(), world.getDeltaY());
+                background.paint(g);
+                world.paint(g);
+                hud.paint(g);
 
-            if (gameState == PRE_STATE) {
-                Image preImage = null;
-                switch (preState) {
-                    case ZERO:
-                        preImage = oneImage;
-                        break;
-                    case ONE:
-                        preImage = oneImage;
-                        break;
-                    case TWO:
-                        preImage = twoImage;
-                        break;
-                    case THREE:
-                        preImage = threeImage;
-                        break;
-                    case START:
-                        preImage = startImage;
-                        break;
-                }
-                g.drawImage(preImage, 160, 120, Graphics.HCENTER | Graphics.VCENTER);
-            }
-
-            if (isGameEnd()) {
-                Image notificationImage = (gameState == WIN_STATE) ? winImage : loseImage;
-                g.drawImage(notificationImage, g.getClipWidth() / 2, g.getClipHeight() / 2, Graphics.HCENTER | Graphics.VCENTER);
-
-                berlin.paintString(g, Integer.toString(curScore), 150, 110, Graphics.LEFT | Graphics.TOP);
-                if (postState >= COMBO) {
-                    berlin.paintString(g, Integer.toString(world.getComboObj().getHighestCombo()), 150, 130, Graphics.LEFT | Graphics.TOP);
-                }
-
-                if (postState >= ANIMAL) {
-                    int life = hud.getLife();
-                    
-                    if (animCounter > ANIM1 ) {
-                        if(life >= 1)
-                            g.drawImage(hud.getAnimal1(), 150, 150, 0);
-                        else
-                            g.drawImage(hud.getAnimal1Fail(), 150, 150, 0);
-                    } 
-                    if (animCounter > ANIM2) {
-                        if(life >= 2)
-                            g.drawImage(hud.getAnimal2(), 170, 150, 0);
-                        else
-                            g.drawImage(hud.getAnimal2Fail(), 170, 150, 0);
+                if (gameState == PRE_STATE) {
+                    Image preImage = null;
+                    switch (preState) {
+                        case ZERO:
+                            preImage = oneImage;
+                            break;
+                        case ONE:
+                            preImage = oneImage;
+                            break;
+                        case TWO:
+                            preImage = twoImage;
+                            break;
+                        case THREE:
+                            preImage = threeImage;
+                            break;
+                        case START:
+                            preImage = startImage;
+                            break;
                     }
-                    if (animCounter > ANIM3) {
-                        if(life >= 3)
-                            g.drawImage(hud.getAnimal3(), 190, 150, 0);
-                        else
-                            g.drawImage(hud.getAnimal3Fail(), 190, 150, 0);
+                    g.drawImage(preImage, 160, 120, Graphics.HCENTER | Graphics.VCENTER);
+                }
+
+                if (isGameEnd()) {
+                    Image notificationImage = (gameState == WIN_STATE) ? winImage : loseImage;
+                    g.drawImage(notificationImage, g.getClipWidth() / 2, g.getClipHeight() / 2, Graphics.HCENTER | Graphics.VCENTER);
+
+                    berlin.paintString(g, Integer.toString(curScore), 150, 110, Graphics.LEFT | Graphics.TOP);
+                    if (postState >= COMBO) {
+                        berlin.paintString(g, Integer.toString(world.getComboObj().getHighestCombo()), 150, 130, Graphics.LEFT | Graphics.TOP);
+                    }
+
+                    if (postState >= ANIMAL) {
+                        int life = hud.getLife();
+
+                        if (animCounter > ANIM1) {
+                            if (life >= 1) {
+                                g.drawImage(hud.getAnimal1(), 150, 150, 0);
+                            } else {
+                                g.drawImage(hud.getAnimal1Fail(), 150, 150, 0);
+                            }
+                        }
+                        if (animCounter > ANIM2) {
+                            if (life >= 2) {
+                                g.drawImage(hud.getAnimal2(), 170, 150, 0);
+                            } else {
+                                g.drawImage(hud.getAnimal2Fail(), 170, 150, 0);
+                            }
+                        }
+                        if (animCounter > ANIM3) {
+                            if (life >= 3) {
+                                g.drawImage(hud.getAnimal3(), 190, 150, 0);
+                            } else {
+                                g.drawImage(hud.getAnimal3Fail(), 190, 150, 0);
+                            }
+                        }
                     }
                 }
             }
@@ -294,37 +304,50 @@ public class MainPlay extends Scene {
                 MainMenu mainMenu = new MainMenu(engine);
                 changeScene(mainMenu);
             }
-        } else if (keyCode == GameCanvas.FIRE && isGameEnd()) {
-            switch (postState) {
-                case SCORE:
-                    curScore = hud.getScore();
-                    break;
-                case COMBO:
-                    break;
-                case ANIMAL:
-                    animCounter = ANIM3 + 1;
-                    break;
-                case CONFIRM:
-                    if (curScore == hud.getScore()) {
-                        profile.setMoney(world.getHUDMoney());
-                        profile.setBestCombo(level-1, Math.max(profile.getBestCombo(level-1), world.getComboObj().getHighestCombo()));
-                        profile.setHighscore(level-1, Math.max(profile.getHighscore(level-1), hud.getScore()));
-                        profile.setSavedAnimals(level-1, Math.max(profile.getSavedAnimals(level-1), hud.getLife()));
-
-                        if (gameState == WIN_STATE) {
-                            profile.setLastLevel(Math.max(profile.getLastLevel(), level));
-                        }
-
-                        MainMenu mainMenu = new MainMenu(engine);
-                        changeScene(mainMenu);
-                    } else {
+        } else if (keyCode == GameCanvas.FIRE) {
+            if (isGameEnd()) {
+                switch (postState) {
+                    case SCORE:
                         curScore = hud.getScore();
+                        break;
+                    case COMBO:
+                        break;
+                    case ANIMAL:
+                        animCounter = ANIM3 + 1;
+                        break;
+                    case CONFIRM:
+                        if (curScore == hud.getScore()) {
+                            profile.setMoney(world.getHUDMoney());
+                            profile.setBestCombo(level - 1, Math.max(profile.getBestCombo(level - 1), world.getComboObj().getHighestCombo()));
+                            profile.setHighscore(level - 1, Math.max(profile.getHighscore(level - 1), hud.getScore()));
+                            profile.setSavedAnimals(level - 1, Math.max(profile.getSavedAnimals(level - 1), hud.getLife()));
+
+                            if (gameState == WIN_STATE) {
+                                profile.setLastLevel(Math.max(profile.getLastLevel(), level));
+                            }
+
+                            MainMenu mainMenu = new MainMenu(engine);
+                            changeScene(mainMenu);
+                        } else {
+                            curScore = hud.getScore();
+                        }
+                        break;
+                }
+            } else {
+
+                if (isTutorial) {
+                    currentTutorialIndex++;
+                    if (currentTutorialIndex > 3) {
+                        isTutorial = false;
                     }
-                    break;
+                    currentTutorialImage = getTutorialImage(currentTutorialIndex);
+                }
+
+
+
             }
 
         }
-
     }
 
     public void updateKeyStates() {
@@ -346,5 +369,17 @@ public class MainPlay extends Scene {
 
     public boolean isGameEnd() {
         return gameState == WIN_STATE || gameState == LOSE_STATE;
+    }
+    private static final String[] tutorials = {
+        "/help_1.png", "/help_2.png", "help_3.png", "help_4.png"};
+
+    private Image getTutorialImage(int index) {
+        Image image = null;
+        try {
+            image = Image.createImage(tutorials[index]);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return image;
     }
 }
